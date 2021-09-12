6e3e7915053d9d039d9025076d72bb877905fbe3
BelezaSystem <otacilio.system@gmail.com>
2021-09-12 12:02:56 -0400

    removido apontamentos desnecessarios

--- lib/Components/bike_grid_item.dart
+++ lib/Components/bike_grid_item.dart
@@ -1,4 +1,3 @@
-import 'package:app_aluguel_bike_modulacao_mvp/Providers/cart_item.dart';
 import 'package:bikes/Models/bike.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
